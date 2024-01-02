be9c9479064d797eac9595430ea8b371ec4f1e48
Suleng18 <90331457+suleng18@users.noreply.github.com>
2024-01-02 23:27:30 +0700

    fix !subscription

--- components/Library.tsx
+++ components/Library.tsx
@@ -27,9 +27,9 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       return authModal.onOpen();
     }
 
-    // if (!subscription) {
-    //   return subscribeModal.onOpen();
-    // }
+    if (!subscription) {
+      return subscribeModal.onOpen();
+    }
 
     return uploadModal.onOpen();
   };
