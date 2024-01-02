4844cee5e77fa3a363ef33c67576618d736690d8
Suleng18 <90331457+suleng18@users.noreply.github.com>
2024-01-02 23:20:23 +0700

    apiVersion

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2022-11-15',
+  apiVersion: '2023-10-16', // Update to a valid and compatible version
   appInfo: {
     name: 'Spotify Clone',
     version: '0.1.0',
